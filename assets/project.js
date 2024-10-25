fc2520c1ac70f3183074f01e1e32dcf623e6a792
Mikie23 <miwuziko@gmail.com>
2024-10-25 00:11:47 -0300

    first commit

---
+++ assets/project.js
@@ -0,0 +1,20 @@
+function avisar(){
+   
+
+let nome = document.getElementById('nome').value;
+if (nome) {
+    alert(`Seu nome é: ${nome}`)
+} else {
+    alert("Digite um nome válido.")
+}
+
+}
+let botao = document.querySelector("#botao");
+
+
+
+botao.addEventListener("click", () =>{
+    avisar()
+})
+
+
